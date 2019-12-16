dd81ff6367aea6c1ba0bf44d030c21f0d5ee9c66
huangqinjin <huangqinjin@gmail.com>
2019-12-17 00:23:53 +0800

    move session manager implementation into session.cpp

--- src/session.cpp
+++ src/session.cpp
@@ -10,11 +10,16 @@
 #include <minapp/handler.hpp>
 #include <minapp/session.hpp>
 #include <minapp/service.hpp>
+#include <minapp/session_manager.hpp>
+#include <minapp/spinlock.hpp>
+
+#include <mutex>
 
 #include <boost/range/iterator_range_core.hpp>
 #include <boost/asio/read.hpp>
 #include <boost/asio/read_until.hpp>
 #include <boost/asio/write.hpp>
+#include <boost/intrusive/set.hpp>
 
 using namespace minapp;
 
@@ -143,6 +148,43 @@ namespace
     const std::string crlf = { '\r', '\n' };
 
     std::atomic_ulong id = ATOMIC_VAR_INIT(1);
+
+
+    struct session_impl : session, boost::intrusive::set_base_hook<boost::intrusive::link_mode<boost::intrusive::safe_link>>
+    {
+        explicit session_impl(service_ptr service);
+        ~session_impl();
+    };
+
+    struct session_id
+    {
+        using type = unsigned long;
+        type operator()(const session& s) const noexcept { return s.id(); }
+    };
+
+    struct manager_impl : session_manager
+    {
+        typedef boost::intrusive::set<session_impl,
+                boost::intrusive::constant_time_size<false>,
+                boost::intrusive::key_of_value<session_id>> session_set;
+
+        spinlock guard;
+        session_set sessions;
+    };
+}
+
+session_impl::session_impl(service_ptr service) : session(std::move(service))
+{
+    auto impl = static_cast<manager_impl*>(this->service()->manager().get());
+    std::lock_guard<spinlock> guard(impl->guard);
+    impl->sessions.insert(*this);
+}
+
+session_impl::~session_impl()
+{
+    auto impl = static_cast<manager_impl*>(this->service()->manager().get());
+    std::lock_guard<spinlock> guard(impl->guard);
+    impl->sessions.erase(impl->sessions.iterator_to(*this));
 }
 
 session::session(service_ptr service)
@@ -564,4 +606,53 @@ void session::read_prefix(std::size_t len)
             read_fixed(data_size);
         }
     }
+}
+
+
+session_manager_ptr session_manager::create()
+{
+    return std::make_shared<manager_impl>();
+}
+
+session_ptr session_manager::create(service_ptr service)
+{
+    return std::make_shared<session_impl>(std::move(service));
+}
+
+session_ptr session_manager::get(unsigned long id)
+{
+    auto impl = static_cast<manager_impl*>(this);
+    std::lock_guard<spinlock> guard(impl->guard);
+    auto iter = impl->sessions.find(id);
+    if(iter != impl->sessions.end())
+        return iter->shared_from_this();
+    return session_ptr{};
+}
+
+std::size_t session_manager::foreach(object::fn<bool(&)(session*)> f)
+{
+    std::size_t c = 0;
+    auto impl = static_cast<manager_impl*>(this);
+    impl->guard.lock();
+    auto iter = impl->sessions.begin();
+    while (iter != impl->sessions.end())
+    {
+        auto session = iter->shared_from_this();
+        if (!session)
+        {
+            iter = impl->sessions.upper_bound(iter->id());
+        }
+        else
+        {
+            impl->guard.unlock();
+            if(!f(session.get())) return c;
+            ++c;
+            auto id = session->id();
+            session = nullptr;
+            impl->guard.lock();
+            iter = impl->sessions.upper_bound(id);
+        }
+    }
+    impl->guard.unlock();
+    return c;
 }
